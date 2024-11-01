17c0c850636a65b3560c419259947f192b5fd89a
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-11-01 09:57:54 +0600

    Basic Data Type of TS added

---
+++ src/basicDataType.ts
@@ -0,0 +1,32 @@
+// BASIC DATA TYPE
+
+// string
+let firstName: string = "Ishtiak";
+
+// number
+let roll: number = 107;
+
+// boolean
+let isAdmin: boolean = true;
+
+// undefined
+let x: undefined = undefined;
+
+// null
+let y: null = null;
+
+// any type
+let d: number;
+d = 107;
+
+// Array
+let frinds: string[] = ["Ishtiak", "Taranumm", "Monica", "Rechel"];
+frinds.push("Sayma");
+
+let eligibleRollList: number[] = [1, 0, 7];
+
+// Tuple -----> array ----> order ----> type of values
+let coordinates: [number, number] = [1, 7];
+
+let ageName: [number, string, boolean] = [107, "Ishtiak", true];
+ageName[0] = 32;

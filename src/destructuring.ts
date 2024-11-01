8a5506e7cb27aa1e398bea48154c025b0922e494
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-11-01 11:57:35 +0600

    Object and Array destructuring added

---
+++ src/destructuring.ts
@@ -0,0 +1,25 @@
+{
+  // object destructing
+  const user = {
+    id: 107,
+    name: {
+      firstName: "Ishtiak",
+      last: "Ahmed",
+      nickName: "Erin",
+    },
+    contactNo: "01737223990",
+    address: "Ulipur, Kurigram",
+  };
+  const {
+    id,
+    name: { firstName },
+    contactNo,
+  } = user;
+
+  //   arrar destructing
+  const myFriends = ["chandler", "Joey", "Ross", "Rachel"];
+  const [a, b, bestFriend] = myFriends;
+
+  const myCartoon = ["Tom", "Jerry", "Oggy", "Olivia", "Jack", "Bob"];
+  const [, , bestCartoon, ...rest] = myCartoon;
+}

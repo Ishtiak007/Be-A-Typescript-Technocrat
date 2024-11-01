8eb1f9b07ad358d5b82adc137399d2ec8a3a9892
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-11-01 16:03:03 +0600

    union and intersection type added

---
+++ src/unionIntersection.ts
@@ -0,0 +1,42 @@
+{
+  // union types
+  type FroentendDeveloper = "fakibazDeveloper" | "juniorDeveloper";
+  type FullStackdDeveloper = "frontendDeveloper" | "expertDeveloper";
+  type Developer = FroentendDeveloper | FullStackdDeveloper;
+
+  const newDeveloper: FroentendDeveloper = "fakibazDeveloper";
+  const newDeveloper2: Developer = "expertDeveloper";
+
+  type User = {
+    name: string;
+    email: string;
+    gender: "male" | "female";
+    bloodGroup: "o+" | "A+" | "AB+";
+    address?: string;
+  };
+
+  const user1: User = {
+    name: "Ishtiak",
+    email: "ishtiakahmed01999@gmail.com",
+    gender: "male",
+    bloodGroup: "o+",
+  };
+
+  //   .............................................
+  // Intersection type
+  type FroentWebDeveloper = {
+    skills: string[];
+    designation1: "Froentend Web Developer";
+  };
+  type BackendWebDeveloper = {
+    skills: string[];
+    designation2: "Backend Web Developer";
+  };
+
+  type FullStackWebDeveloper = FroentWebDeveloper & BackendWebDeveloper;
+  const fullDeveloper: FullStackWebDeveloper = {
+    skills: ["HTML", "CSS", "Javascript", "Typescript", "MERN"],
+    designation1: "Froentend Web Developer",
+    designation2: "Backend Web Developer",
+  };
+}

bdd95f0e524fa6ef3ac3cb4fa81f931b8a1d0372
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-11-05 19:29:41 +0600

    practice added for inheritence in opp

--- module3/inheritenceInOpp.ts
+++ module3/inheritenceInOpp.ts
@@ -1,53 +1,101 @@
-{
-  // oop- inheritence
+// {
+//   // oop- inheritence
 
+//   class Person {
+//     name: string;
+//     age: number;
+//     address: string;
+
+//     constructor(name: string, age: number, address: string) {
+//       this.name = name;
+//       this.age = age;
+//       this.address = address;
+//     }
+//     // getSleep(numOfHours: number) {
+//     //   console.log(
+//     //     `Name : ${this.name}--- Age : ${this.age}--- Address : ${this.address} and He sleeps : ${numOfHours}`
+//     //   );
+//     // }
+//   }
+
+//   class Student extends Person {
+//     constructor(name: string, age: number, address: string) {
+//       super(name, age, address);
+//     }
+//     getSleep(numOfHours: number) {
+//       console.log(
+//         `Name : ${this.name}--- Age : ${this.age}--- Address : ${this.address} and He sleeps : ${numOfHours}`
+//       );
+//     }
+//   }
+//   class Teacher extends Person {
+//     designation: string;
+//     constructor(
+//       name: string,
+//       age: number,
+//       address: string,
+//       designation: string
+//     ) {
+//       super(name, age, address);
+//       this.designation = designation;
+//     }
+//     takeClass(numOfClass: number) {
+//       console.log(
+//         `Name : ${this.name}--- Age : ${this.age}--- Address : ${this.address} and He will take class : ${numOfClass}`
+//       );
+//     }
+//   }
+
+//   const student1 = new Student("Ishtiak", 21, "UK Kurigram");
+//   student1.getSleep(20);
+//   const teacher1 = new Teacher("Jhonkar Mahbub", 41, "Texas", "Professor");
+//   teacher1.takeClass(7);
+// }
+{
   class Person {
     name: string;
     age: number;
     address: string;
-
     constructor(name: string, age: number, address: string) {
       this.name = name;
       this.age = age;
       this.address = address;
     }
-    // getSleep(numOfHours: number) {
-    //   console.log(
-    //     `Name : ${this.name}--- Age : ${this.age}--- Address : ${this.address} and He sleeps : ${numOfHours}`
-    //   );
-    // }
   }
-
   class Student extends Person {
     constructor(name: string, age: number, address: string) {
-      super(name, age, address);
+      {
+        super(name, age, address);
+      }
     }
-    getSleep(numOfHours: number) {
+    getSleep(hour: number) {
       console.log(
-        `Name : ${this.name}--- Age : ${this.age}--- Address : ${this.address} and He sleeps : ${numOfHours}`
+        `${this.name} sleeps ${hour} h and his age${this.age} ; address: ${this.address}`
       );
     }
   }
   class Teacher extends Person {
-    designation: string;
+    dasignation: string;
     constructor(
       name: string,
       age: number,
       address: string,
-      designation: string
+      dasignation: string
     ) {
-      super(name, age, address);
-      this.designation = designation;
+      {
+        super(name, age, address);
+      }
+      this.dasignation = dasignation;
     }
-    takeClass(numOfClass: number) {
+    takenClass(classHour: number) {
       console.log(
-        `Name : ${this.name}--- Age : ${this.age}--- Address : ${this.address} and He will take class : ${numOfClass}`
+        `${this.name} sir will take class ${classHour} h and his age: ${this.age} ----dasignation is ${this.dasignation}`
       );
     }
   }
 
-  const student1 = new Student("Ishtiak", 21, "UK Kurigram");
-  student1.getSleep(20);
-  const teacher1 = new Teacher("Jhonkar Mahbub", 41, "Texas", "Professor");
-  teacher1.takeClass(7);
+  const student = new Student("Ishtiak", 23, "Dhaka");
+  const teacher = new Teacher("Dilip", 30, "Kurigram", "Professor");
+  student.getSleep(19);
+  teacher.takenClass(23);
 }

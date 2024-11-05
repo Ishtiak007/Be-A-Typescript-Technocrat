a94f08840b9e15ccdb1dff00c8e7f87a5df4bd0b
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-11-05 21:38:28 +0600

    typeGuardUsing Typeof and In functionality added

--- module3/inheritenceInOpp.ts
+++ module3/inheritenceInOpp.ts
@@ -1,56 +1,51 @@
-// {
-//   // oop- inheritence
+{
+  // oop- inheritence
 
-//   class Person {
-//     name: string;
-//     age: number;
-//     address: string;
+  class Person {
+    name: string;
+    age: number;
+    address: string;
 
-//     constructor(name: string, age: number, address: string) {
-//       this.name = name;
-//       this.age = age;
-//       this.address = address;
-//     }
-//     // getSleep(numOfHours: number) {
-//     //   console.log(
-//     //     `Name : ${this.name}--- Age : ${this.age}--- Address : ${this.address} and He sleeps : ${numOfHours}`
-//     //   );
-//     // }
-//   }
+    constructor(name: string, age: number, address: string) {
+      this.name = name;
+      this.age = age;
+      this.address = address;
+    }
+  }
 
-//   class Student extends Person {
-//     constructor(name: string, age: number, address: string) {
-//       super(name, age, address);
-//     }
-//     getSleep(numOfHours: number) {
-//       console.log(
-//         `Name : ${this.name}--- Age : ${this.age}--- Address : ${this.address} and He sleeps : ${numOfHours}`
-//       );
-//     }
-//   }
-//   class Teacher extends Person {
-//     designation: string;
-//     constructor(
-//       name: string,
-//       age: number,
-//       address: string,
-//       designation: string
-//     ) {
-//       super(name, age, address);
-//       this.designation = designation;
-//     }
-//     takeClass(numOfClass: number) {
-//       console.log(
-//         `Name : ${this.name}--- Age : ${this.age}--- Address : ${this.address} and He will take class : ${numOfClass}`
-//       );
-//     }
-//   }
+  class Student extends Person {
+    constructor(name: string, age: number, address: string) {
+      super(name, age, address);
+    }
+    getSleep(numOfHours: number) {
+      console.log(
+        `Name : ${this.name}--- Age : ${this.age}--- Address : ${this.address} and He sleeps : ${numOfHours}`
+      );
+    }
+  }
+  class Teacher extends Person {
+    designation: string;
+    constructor(
+      name: string,
+      age: number,
+      address: string,
+      designation: string
+    ) {
+      super(name, age, address);
+      this.designation = designation;
+    }
+    takeClass(numOfClass: number) {
+      console.log(
+        `Name : ${this.name}--- Age : ${this.age}--- Address : ${this.address} and He will take class : ${numOfClass}`
+      );
+    }
+  }
 
-//   const student1 = new Student("Ishtiak", 21, "UK Kurigram");
-//   student1.getSleep(20);
-//   const teacher1 = new Teacher("Jhonkar Mahbub", 41, "Texas", "Professor");
-//   teacher1.takeClass(7);
-// }
+  const student1 = new Student("Ishtiak", 21, "UK Kurigram");
+  student1.getSleep(20);
+  const teacher1 = new Teacher("Jhonkar Mahbub", 41, "Texas", "Professor");
+  teacher1.takeClass(7);
+}
 {
   class Person {
     name: string;

451d560ba4d8a74d9823ac83573fd1a1e6e23b7c
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-11-06 10:28:58 +0600

    practices updated for getter and setter topic

--- module3/getterAndSetter.ts
+++ module3/getterAndSetter.ts
@@ -24,3 +24,26 @@
   const myBalance = goribManusherAccount.balance; //property er moto kore
   console.log(myBalance);
 }
+//practices.........................................................
+{
+  class BankAccount2 {
+    public readonly id: number;
+    public name: string;
+    protected _balance: number;
+    constructor(id: number, name: string, _balance: number) {
+      this.id = id;
+      this.name = name;
+      this._balance = _balance;
+    }
+    get balance() {
+      return this._balance;
+    }
+    set deposit(amount: number) {
+      this._balance = this.balance + amount;
+    }
+  }
+  const myBankAccount = new BankAccount2(107, "Ishtiak", 1000);
+  myBankAccount.deposit = 1000;
+  const myBalance = myBankAccount.balance;
+  console.log(myBalance);
+}

5d4ea8de6c3236b859d33746843537b887e48529
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-11-06 11:25:11 +0600

    implementing polymorphism

--- module3/statics.ts
+++ module3/statics.ts
@@ -24,3 +24,21 @@
   console.log(Counter.increment());
   console.log(Counter.increment());
 }
+{
+  class Counter {
+    static count: number = 0;
+    static increment() {
+      return (Counter.count = Counter.count + 1);
+    }
+    static decrement() {
+      return (Counter.count = Counter.count - 1);
+    }
+  }
+  console.log(Counter.decrement());
+  console.log(Counter.decrement());
+  console.log(Counter.decrement());
+  console.log(Counter.decrement());
+  console.log(Counter.decrement());
+  console.log(Counter.decrement());
+  console.log(Counter.decrement());
+}

6d0a05b9a6ae28f5fc6255167de6fea279a325c7
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-11-06 13:21:32 +0600

    abstraction.ts file updated with rest code

--- module3/abstraction.ts
+++ module3/abstraction.ts
@@ -1,11 +1,12 @@
 {
   //abstraction
+  //idea
   interface Vehicle1 {
     startEngine(): void;
     stopEngine(): void;
     move(): void;
   }
-  class Car implements Vehicle1 {
+  class Car1 implements Vehicle1 {
     startEngine(): void {
       console.log(`I am starting the car engine`);
     }
@@ -20,6 +21,31 @@
     }
   }
 
-  const tyotaCar = new Car();
+  const tyotaCar = new Car1();
   tyotaCar.startEngine();
+
+  // abstract class ------for idea
+  abstract class Car2 {
+    abstract startEngine(): void;
+    abstract stopEngine(): void;
+    abstract move(): void;
+    test(): void {
+      console.log(`I am just testing`);
+    }
+  }
+
+  class Tpyota extends Car2 {
+    startEngine(): void {
+      console.log("I am starting the car engine");
+    }
+    stopEngine(): void {
+      console.log("I am stoping the car engine");
+    }
+    move(): void {
+      console.log("I am moving the car engine");
+    }
+  }
+
+  //   const hondacar = new Car2();
+  //   hondacar.startEngine();
 }

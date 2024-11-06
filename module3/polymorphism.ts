669c83db8600deea6568b3df86f7012e02d65b4c
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-11-06 12:48:53 +0600

    polymorphism file updated with code

--- module3/polymorphism.ts
+++ module3/polymorphism.ts
@@ -27,4 +27,48 @@
   getSleepingHours(person1);
   getSleepingHours(person2);
   getSleepingHours(person3);
+
+  //   ..........................
+  class Shape {
+    getArea(): number {
+      return 0;
+    }
+  }
+  //pi*r*r
+  class Circle extends Shape {
+    radius: number;
+
+    constructor(radius: number) {
+      super();
+      this.radius = radius;
+    }
+    getArea(): number {
+      return Math.PI * this.radius * this.radius;
+    }
+  }
+  //height width
+  class Rectengle extends Shape {
+    height: number;
+    width: number;
+    constructor(height: number, width: number) {
+      super();
+      this.height = height;
+      this.width = width;
+    }
+    getArea(): number {
+      return this.height * this.width;
+    }
+  }
+
+  const getShapeArea = (param: Shape) => {
+    console.log(param.getArea());
+  };
+
+  const shape1 = new Shape();
+  const shape2 = new Circle(100);
+  const shape3 = new Rectengle(10, 20);
+
+  getShapeArea(shape1);
+  getShapeArea(shape2);
+  getShapeArea(shape3);
 }
